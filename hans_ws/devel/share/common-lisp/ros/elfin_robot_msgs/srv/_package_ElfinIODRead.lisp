d876a369184d3eac7b16924b712d100f714969a5
antwoor <geroev_sasha@mail.ru>
2024-12-18 08:18:00 +0300

    cpp packages has been built properelly & gitigonre upd

--- hans_ws/devel/share/common-lisp/ros/elfin_robot_msgs/srv/_package_ElfinIODRead.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package elfin_robot_msgs-srv)
-(cl:export '(DATA-VAL
-          DATA
-          DIGITAL_INPUT-VAL
-          DIGITAL_INPUT
-))
--- hans_ws/devel/share/common-lisp/ros/elfin_robot_msgs/srv/_package_SetFloat64.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package elfin_robot_msgs-srv)
-(cl:export '(DATA-VAL
-          DATA
-          SUCCESS-VAL
-          SUCCESS
-          MESSAGE-VAL
-          MESSAGE
-))
