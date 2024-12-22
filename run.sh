55dacffb16cd88db6edde6a91de33309be065a7f
antwoor <geroev_sasha@mail.ru>
2024-12-22 21:14:33 +0300

    GUI XHOST fixed

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
+xhost local:root
 docker run -it \
     --rm \
     --network host \
-    --env DISPLAY \
+    --env DISPLAY=$DISPLAY \
     --gpus=all \
     --volume /tmp/.X11-unix:/tmp/.X11-unix \
     --volume $HOME/Solovov/robo-painter:/workspace \
